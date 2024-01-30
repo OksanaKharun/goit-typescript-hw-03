1595adf4c29c4f45a46770a5599097e118f78244
Oksana Kharun <oksana0202g@gmail.com>
2024-01-30 14:57:16 +0000

    Update

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
-const key = new Key();
+class Key {
+    private signature: number = Math.random();
 
+    getSignature(): number{
+    return this.signature
+    }
+}
+
+class Person{
+    private key: Key;
+
+    constructor(key: Key) {
+    this.key = key;
+  }
+    getKey(): Key{
+    return this.key
+    }
+    
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+
+    constructor(protected key: Key) {
+    }
+
+   abstract openDoor(person: Person): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log('Двері відчинені');
+    } else {
+      console.log('Двері зачинені');
+    }
+  }
+}
+
+class MyHouse extends House {
+  openDoor(person: Person): void {
+    if (person.getKey().getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('Двері відчиняються');
+    } else {
+      console.log('Двері зачиняються');
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
-house.openDoor(person.getKey());
-
+house.openDoor(person);
 house.comeIn(person);
+
+
 
 
 export {};
